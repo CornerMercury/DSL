e1a3bf394fa855faaa4d7fe52550a377c6586271
CornerMercury <corner@mercury.com>
2026-02-02 17:54:14 +0000

    feat: add adding and subtracting to parsing

--- src/main/DSL/frontend/ast.scala
+++ src/main/DSL/frontend/ast.scala
@@ -1,19 +1,13 @@
 package DSL.frontend
 
-object ast {
-  
-  // Base trait for all nodes
+object AST {
   sealed trait AstNode
 
-  // We define Expr as a trait to allow future expansion 
-  // (e.g. if you want to add "d6 + 5" later)
   sealed trait Expr extends AstNode
 
-  /**
-    * Represents a Die roll.
-    * Example: d6 -> Die(6)
-    * Example: d20 -> Die(20)
-    */
+  case class IntLiteral(value: Int) extends Expr
   case class Die(sides: Int) extends Expr
+  case class Add(left: Expr, right: Expr) extends Expr
+  case class Sub(left: Expr, right: Expr) extends Expr
   
 }
--- src/main/DSL/frontend/parser.scala
+++ src/main/DSL/frontend/parser.scala
@@ -1,15 +1,15 @@
 package DSL.frontend
 
 import parsley.Parsley
-import parsley.Parsley.{eof}
-import parsley.character.{digit, char, string, item}
-import parsley.combinator.{many, some}
 import parsley.errors.ErrorBuilder
 import parsley.{Success, Failure, Result}
 
+// 1. Import Expression parsing tools
+import parsley.expr.{precedence, Ops, InfixL}
+
 import DSL.frontend.lexer.implicits.implicitSymbol
 import DSL.frontend.lexer.{integer, fully}
-import DSL.frontend.ast._
+import DSL.frontend.AST._
 
 object parser {
   def parse[Err: ErrorBuilder](input: String): Result[String, AstNode] = {
@@ -19,13 +19,31 @@ object parser {
     }
   }
 
-  private lazy val parser: Parsley[AstNode] = fully(die)
+  // The top-level parser is now an expression
+  private lazy val parser: Parsley[AstNode] = fully(expr)
 
-  /********************************* 
-    Dice Parsers
-    ******************************** */
+  /** ******************************* 
+    * Expression Parser (Precedence)
+    * ******************************* */
+  
+  private lazy val expr: Parsley[Expr] = precedence[Expr](atom)(
+    Ops(InfixL)(
+      "+" #> Add.apply,
+      "-" #> Sub.apply 
+    )
+  )
+
+  /** ******************************* 
+    * Atoms (Basic Units)
+    * ******************************* */
+
+  private lazy val atom: Parsley[Expr] = die <|> literal
 
   private lazy val die: Parsley[Die] = {
     ("d" ~> integer).map(sides => Die(sides))
+  }
+
+  private lazy val literal: Parsley[IntLiteral] = {
+    integer.map(n => IntLiteral(n))
   }
 }
